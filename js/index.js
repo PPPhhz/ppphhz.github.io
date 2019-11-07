31392683005d814b5a703ec8725bc8e6b312b760
phhz <18851908011@qq.com>
2019-11-07 18:28:09 +0800

    添加菜单按钮，点击按钮左侧楼层fadeIn/Out

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
-$(function(){$(window).scroll(function(){var e=$("body").scrollTop()||$("html").scrollTop();e>=150?$(".left-side").stop().fadeIn(500):$(".left-side").stop().fadeOut(500),$(".floor").each(function(t,o){var a=$(o).offset().top;$(o).height();e>=a-1&&$(".footer li").eq(t).addClass("cur").siblings().removeClass("cur")})}),$(".footer li").click(function(){$(this).addClass("cur").siblings().removeClass("cur");var e=$(this).index(),t=$(".floor").eq(e).offset().top;$("body , html").animate({scrollTop:t},1e3)}),$(".hat").click(function(){$(".getHat").fadeOut(1e3),$(".gotHat").delay(1e3).fadeIn(1e3)}),$(".hat").mouseenter(function(){$(".getHat").fadeOut(1e3),$(".gotHat").delay(1e3).fadeIn(1e3)})});
+$(function() {
+    $(".icon-caidan1").click(function () {
+        $(".left-side").stop().toggle(500);
+    });
+    $(window).scroll(function () {
+        var offset=$('body').scrollTop() || $('html').scrollTop();
+        //     // 获取网页高度
+        //     var $winHeight = $(window).height();
+        //     //判断是否滚出一屏
+
+        // 2.2遍历拿所有楼层
+        $(".floor").each(function (idx, ele) {
+            // 2.2.1获得当前楼层距离顶部距离
+            var $floorOffset = $(ele).offset().top;
+            // 2.2.2获取当前楼层高度
+            var $floorHeight = $(ele).height();
+            //判断滚动出去的距离是否大于当前楼层
+            if(offset >=$floorOffset-1){
+                // 获取对应楼层按钮
+                var $li = $(".footer li").eq(idx);
+                // 自动设置楼层按钮背景为当前楼层
+                $li.addClass("cur").siblings().removeClass("cur");
+            }
+        });
+    });
+
+    // 1.监听楼层点击事件
+    $(".footer li").click(function () {
+        // 1.0设置当前点击楼层背景颜色
+        $(this).addClass("cur").siblings().removeClass("cur");
+        // 1.1.获取当前点击的楼层索引
+        var index = $(this).index();
+        // 1.2获取索引对应楼层距离顶部的距离
+        var offset = $(".floor").eq(index).offset().top;
+        // 1.3让网页滚动到对应楼层的位置
+        $('body , html').animate({scrollTop: offset}, 1000);
+    });
+
+    // 给reol戴帽子
+    $(".hat").click(function () {
+        $(".getHat").fadeOut(1000);
+        $(".gotHat").delay(1000).fadeIn(1000);
+    });
+    $(".hat").mouseenter(function () {
+        $(".getHat").fadeOut(1000);
+        $(".gotHat").delay(1000).fadeIn(1000);
+    })
+});
